cc998b4fd5e6ec031910e9307d10e56f4f3db345
Adrian Groh <adrian.groh@t-online.de>
2023-12-05 15:32:56 +0100

    Clean up day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,20 +4,21 @@ struct Map {
 }
 
 impl Map {
-    fn new() -> Self {
-        Self {
-            maps: vec![],
-        }
-    }
     fn from_block(block: &str) -> Self {
-        let mut result = Map::new();
-        for range_str in block.lines().skip(1) {
-            let range_values: Vec<u32> = range_str.split(' ').map(|n| n.parse().unwrap()).collect();
-            result
-                .maps
-                .push((range_values[0], range_values[1], range_values[2]))
+        Self {
+            maps: block
+                .lines()
+                .skip(1)
+                .map(|l| {
+                    let mut val_iter = l.split(' ').map(|n| n.parse().unwrap());
+                    (
+                        val_iter.next().unwrap(),
+                        val_iter.next().unwrap(),
+                        val_iter.next().unwrap(),
+                    )
+                })
+                .collect(),
         }
-        result
     }
     fn get_mapped_value(&self, src: u32) -> u32 {
         for map in &self.maps {
@@ -30,57 +31,47 @@ impl Map {
 }
 
 fn parse(input: &str) -> (Vec<u32>, Vec<Map>) {
-    let seeds: Vec<u32> = input
-        .lines()
-        .next()
-        .unwrap()
-        .split(':')
-        .last()
-        .unwrap()
-        .trim()
-        .split(' ')
-        .map(|n| n.parse().unwrap())
-        .collect();
-    let mut maps: Vec<Map> = vec![];
-    for block in input.split("\n\n").skip(1) {
-        maps.push(Map::from_block(block));
+    (
+        input
+            .lines()
+            .next()
+            .unwrap()
+            .split(':')
+            .last()
+            .unwrap()
+            .trim()
+            .split(' ')
+            .map(|n| n.parse().unwrap())
+            .collect(),
+        input.split("\n\n").skip(1).map(Map::from_block).collect(),
+    )
+}
+
+fn map_to_end(seed: u32, maps: &[Map]) -> u32 {
+    let mut current_val = seed;
+    for map in maps {
+        current_val = map.get_mapped_value(current_val);
     }
-    (seeds, maps)
+    current_val
 }
 
 fn part1(seeds: &[u32], maps: &[Map]) -> u32 {
-    let mut current_min = u32::MAX;
-    for seed in seeds {
-        let mut current_val = *seed;
-        for map in maps {
-            current_val = map.get_mapped_value(current_val);
-        }
-        if current_val < current_min {
-            current_min = current_val;
-        }
-    }
-    current_min
+    seeds
+        .iter()
+        .map(|seed| map_to_end(*seed, maps))
+        .min()
+        .unwrap()
 }
 
 fn part2(seeds: &[u32], maps: &[Map]) -> u32 {
-    let seed_ranges = seeds
+    seeds
         .iter()
         .zip(seeds.iter().skip(1))
         .step_by(2)
-        .map(|(start, length)| *start..start + length);
-    let mut current_min = u32::MAX;
-    for seed_range in seed_ranges {
-        for seed in seed_range {
-            let mut current_val = seed;
-            for map in maps {
-                current_val = map.get_mapped_value(current_val);
-            }
-            if current_val < current_min {
-                current_min = current_val;
-            }
-        }
-    }
-    current_min
+        .flat_map(|(start, length)| *start..start + length)
+        .map(|seed| map_to_end(seed, maps))
+        .min()
+        .unwrap()
 }
 
 fn main() {
