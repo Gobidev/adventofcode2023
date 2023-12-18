a3f06822fc4a6cff21746c92904a86ea36d90260
Adrian Groh <adrian.groh@t-online.de>
2023-12-18 20:49:10 +0100

    Add day18 part2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -5,8 +5,6 @@ enum Direction {
     Left,
     Right,
 }
-use std::collections::HashSet;
-
 use Direction::*;
 
 impl Direction {
@@ -16,6 +14,15 @@ impl Direction {
             'D' => Down,
             'L' => Left,
             'R' => Right,
+            _ => panic!(),
+        }
+    }
+    fn from_color(col: u32) -> Self {
+        match col & 15 {
+            0 => Right,
+            1 => Down,
+            2 => Left,
+            3 => Up,
             _ => panic!(),
         }
     }
@@ -62,94 +69,50 @@ fn parse(input_str: &str) -> Vec<Instruction> {
     input_str.lines().map(Instruction::from_line).collect()
 }
 
-fn instruction_positions(
-    current_position: &Position,
-    direction: &Direction,
-    distance: u32,
-) -> Vec<Position> {
-    let mut new_positions = vec![];
-    let direction_pos = direction.as_position();
-    for i in 0..=distance {
-        new_positions.push((
-            current_position.0 + i as isize * direction_pos.0,
-            current_position.1 + i as isize * direction_pos.1,
-        ));
+fn shoelace(positions: &[Position]) -> isize {
+    let mut prev_point = positions.first().unwrap();
+    let mut total_area: isize = 0;
+    for position in positions.iter().skip(1) {
+        total_area += (prev_point.0 + position.0) * (prev_point.1 - position.1);
+        prev_point = position;
     }
-    new_positions
+    total_area.abs() / 2
 }
 
-fn positions_to_map(positions: &HashSet<Position>) -> Vec<Vec<char>> {
-    let min_y = positions.iter().map(|(y, _)| y).min().unwrap();
-    let max_y = positions.iter().map(|(y, _)| y).max().unwrap();
-    let min_x = positions.iter().map(|(_, x)| x).min().unwrap();
-    let max_x = positions.iter().map(|(_, x)| x).max().unwrap();
-    let mut res = vec![];
-    for y_idx in *min_y..=*max_y {
-        let mut row = vec![];
-        for x_idx in *min_x..=*max_x {
-            if positions.contains(&(y_idx, x_idx)) {
-                row.push('#');
-            } else {
-                row.push('.');
-            }
-        }
-        res.push(row);
-    }
-    res
-}
-
-fn fill_map(map: &mut [Vec<char>], pos: Position) {
-    if map[pos.0 as usize][pos.1 as usize] == '.' {
-        map[pos.0 as usize][pos.1 as usize] = '#';
-        fill_map(map, (pos.0 + 1, pos.1));
-        fill_map(map, (pos.0 - 1, pos.1));
-        fill_map(map, (pos.0, pos.1 + 1));
-        fill_map(map, (pos.0, pos.1 - 1));
-    }
-}
-
-#[allow(dead_code)]
-fn print_map(map: &[Vec<char>]) {
-    println!(
-        "{}",
-        map.iter()
-            .map(|l| l.iter().collect::<String>() + "\n")
-            .collect::<String>()
-    );
-}
-
-fn part1(input: &[Instruction]) -> usize {
-    let mut digged_positions: HashSet<Position> = HashSet::new();
+fn calc_area(instructions: &[Instruction], part2: bool) -> isize {
+    let mut corner_positions: Vec<Position> = vec![(0, 0)];
     let mut current_position = (0, 0);
-    for instruction in input {
-        let new_positions = instruction_positions(
-            &current_position,
-            &instruction.direction,
-            instruction.distance,
+    let mut perimiter = 0;
+    let mut direction;
+    let mut distance;
+    for instruction in instructions {
+        if part2 {
+            direction = Direction::from_color(instruction.color).as_position();
+            distance = (instruction.color >> 4) as isize;
+        } else {
+            direction = instruction.direction.as_position();
+            distance = instruction.distance as isize;
+        }
+        perimiter += distance;
+        current_position = (
+            current_position.0 + distance * direction.0,
+            current_position.1 + distance * direction.1,
         );
-        current_position = *new_positions.last().unwrap();
-        digged_positions.extend(&mut new_positions.iter());
+        corner_positions.push(current_position);
     }
-    let mut map = positions_to_map(&digged_positions);
-    let start_pos = map
-        .get(1)
-        .unwrap()
-        .iter()
-        .enumerate()
-        .find(|(_, c)| c == &&'#')
-        .map(|(i, _)| (1isize, (i + 1) as isize))
-        .unwrap();
-    fill_map(&mut map, start_pos);
-    map.iter()
-        .map(|l| {
-            l.iter()
-                .map(|c| if c == &'#' { 1 } else { 0 })
-                .sum::<usize>()
-        })
-        .sum()
+    shoelace(&corner_positions) + perimiter / 2 + 1
+}
+
+fn part1(input: &[Instruction]) -> isize {
+    calc_area(input, false)
+}
+
+fn part2(input: &[Instruction]) -> isize {
+    calc_area(input, true)
 }
 
 fn main() {
     let input = parse(include_str!("../input.txt"));
     println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
